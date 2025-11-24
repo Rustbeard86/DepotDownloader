321f56dfd59256f0f91d3eb17a0ef81005a73bcc
rust86 <nope@nope.mail>
2025-11-24 07:48:51 -0500

    Refactor ProtoManifest and update .editorconfig
    Replaced `.editorconfig` with `.editorconfig.unused`, retaining the same content but marking it as inactive.
    
    Refactored `ProtoManifest.cs` to modernize the codebase:
    - Removed the file header comment referencing `LICENSE`.
    - Updated `ProtoManifest` class to use modern C# features:
      - Replaced `new List<FileData>()` with `[]` for `Files`.
      - Added `ArgumentNullException.ThrowIfNull` for validation.
      - Refactored `FileData` and `ChunkData` classes with inline initializers and record-like constructors.
      - Improved `LoadFromFile` and `SaveToFile` methods with `using` declarations.
      - Enhanced `ConvertToSteamManifest` to handle null `Chunks` gracefully.
    - Renamed `ID` to `Id` for naming consistency.
    
    These changes improve readability, maintainability, and adherence to modern C# standards.

--- DepotDownloader/ProtoManifest.cs
+++ DepotDownloader/ProtoManifest.cs
@@ -1,6 +1,3 @@
-// This file is subject to the terms and conditions defined
-// in file 'LICENSE', which is part of this source code package.
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,187 +7,185 @@ using System.Text;
 using ProtoBuf;
 using SteamKit2;
 
-namespace DepotDownloader
+namespace DepotDownloader;
+
+[ProtoContract]
+internal class ProtoManifest
 {
-    [ProtoContract]
-    class ProtoManifest
+    // Proto ctor
+    private ProtoManifest()
     {
-        // Proto ctor
-        private ProtoManifest()
+        Files = [];
+    }
+
+    public ProtoManifest(DepotManifest sourceManifest, ulong id) : this()
+    {
+        ArgumentNullException.ThrowIfNull(sourceManifest);
+
+        sourceManifest.Files?.ForEach(f => Files.Add(new FileData(f)));
+
+        Id = id;
+        CreationTime = sourceManifest.CreationTime;
+    }
+
+    [ProtoMember(1)] public List<FileData> Files { get; private set; }
+
+    [ProtoMember(2)] public ulong Id { get; private set; }
+
+    [ProtoMember(3)] public DateTime CreationTime { get; private set; }
+
+    public static ProtoManifest LoadFromFile(string filename, out byte[] checksum)
+    {
+        if (!File.Exists(filename))
         {
-            Files = [];
+            checksum = null;
+            return null;
         }
 
-        public ProtoManifest(DepotManifest sourceManifest, ulong id) : this()
+        using var ms = new MemoryStream();
+        using (var fs = File.Open(filename, FileMode.Open))
+        using (var ds = new DeflateStream(fs, CompressionMode.Decompress))
         {
-            sourceManifest.Files.ForEach(f => Files.Add(new FileData(f)));
-            ID = id;
-            CreationTime = sourceManifest.CreationTime;
+            ds.CopyTo(ms);
         }
 
-        [ProtoContract]
-        public class FileData
+        checksum = SHA1.HashData(ms.ToArray());
+
+        ms.Seek(0, SeekOrigin.Begin);
+        return Serializer.Deserialize<ProtoManifest>(ms);
+    }
+
+    public void SaveToFile(string filename, out byte[] checksum)
+    {
+        using var ms = new MemoryStream();
+        Serializer.Serialize(ms, this);
+
+        checksum = SHA1.HashData(ms.ToArray());
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+        using var fs = File.Open(filename, FileMode.Create);
+        using var ds = new DeflateStream(fs, CompressionMode.Compress);
+        ms.CopyTo(ds);
+    }
+
+    public DepotManifest ConvertToSteamManifest(uint depotId)
+    {
+        ulong uncompressedSize = 0, compressedSize = 0;
+        var newManifest = new DepotManifest
         {
-            // Proto ctor
-            private FileData()
-            {
-                Chunks = [];
-            }
+            Files = new List<DepotManifest.FileData>(Files.Count)
+        };
 
-            public FileData(DepotManifest.FileData sourceData) : this()
-            {
-                FileName = sourceData.FileName;
-                sourceData.Chunks.ForEach(c => Chunks.Add(new ChunkData(c)));
-                Flags = sourceData.Flags;
-                TotalSize = sourceData.TotalSize;
-                FileHash = sourceData.FileHash;
-            }
-
-            [ProtoMember(1)]
-            public string FileName { get; internal set; }
-
-            /// <summary>
-            /// Gets the chunks that this file is composed of.
-            /// </summary>
-            [ProtoMember(2)]
-            public List<ChunkData> Chunks { get; private set; }
-
-            /// <summary>
-            /// Gets the file flags
-            /// </summary>
-            [ProtoMember(3)]
-            public EDepotFileFlag Flags { get; private set; }
-
-            /// <summary>
-            /// Gets the total size of this file.
-            /// </summary>
-            [ProtoMember(4)]
-            public ulong TotalSize { get; private set; }
-
-            /// <summary>
-            /// Gets the hash of this file.
-            /// </summary>
-            [ProtoMember(5)]
-            public byte[] FileHash { get; private set; }
-        }
-
-        [ProtoContract(SkipConstructor = true)]
-        public class ChunkData
+        foreach (var file in Files)
         {
-            public ChunkData(DepotManifest.ChunkData sourceChunk)
-            {
-                ChunkID = sourceChunk.ChunkID;
-                Checksum = BitConverter.GetBytes(sourceChunk.Checksum);
-                Offset = sourceChunk.Offset;
-                CompressedLength = sourceChunk.CompressedLength;
-                UncompressedLength = sourceChunk.UncompressedLength;
-            }
+            var fileNameHash =
+                SHA1.HashData(Encoding.UTF8.GetBytes(file.FileName.Replace('/', '\\').ToLowerInvariant()));
+            var chunkCount = file.Chunks?.Count ?? 0;
+            var newFile = new DepotManifest.FileData(file.FileName, fileNameHash, file.Flags, file.TotalSize,
+                file.FileHash, null!, false, chunkCount);
 
-            /// <summary>
-            /// Gets the SHA-1 hash chunk id.
-            /// </summary>
-            [ProtoMember(1)]
-            public byte[] ChunkID { get; private set; }
-
-            /// <summary>
-            /// Gets the expected Adler32 checksum of this chunk.
-            /// </summary>
-            [ProtoMember(2)]
-            public byte[] Checksum { get; private set; }
-
-            /// <summary>
-            /// Gets the chunk offset.
-            /// </summary>
-            [ProtoMember(3)]
-            public ulong Offset { get; private set; }
-
-            /// <summary>
-            /// Gets the compressed length of this chunk.
-            /// </summary>
-            [ProtoMember(4)]
-            public uint CompressedLength { get; private set; }
-
-            /// <summary>
-            /// Gets the decompressed length of this chunk.
-            /// </summary>
-            [ProtoMember(5)]
-            public uint UncompressedLength { get; private set; }
-        }
-
-        [ProtoMember(1)]
-        public List<FileData> Files { get; private set; }
-
-        [ProtoMember(2)]
-        public ulong ID { get; private set; }
-
-        [ProtoMember(3)]
-        public DateTime CreationTime { get; private set; }
-
-        public static ProtoManifest LoadFromFile(string filename, out byte[] checksum)
-        {
-            if (!File.Exists(filename))
-            {
-                checksum = null;
-                return null;
-            }
-
-            using var ms = new MemoryStream();
-            using (var fs = File.Open(filename, FileMode.Open))
-            using (var ds = new DeflateStream(fs, CompressionMode.Decompress))
-                ds.CopyTo(ms);
-
-            checksum = SHA1.HashData(ms.ToArray());
-
-            ms.Seek(0, SeekOrigin.Begin);
-            return Serializer.Deserialize<ProtoManifest>(ms);
-        }
-
-        public void SaveToFile(string filename, out byte[] checksum)
-        {
-            using var ms = new MemoryStream();
-            Serializer.Serialize(ms, this);
-
-            checksum = SHA1.HashData(ms.ToArray());
-
-            ms.Seek(0, SeekOrigin.Begin);
-
-            using var fs = File.Open(filename, FileMode.Create);
-            using var ds = new DeflateStream(fs, CompressionMode.Compress);
-            ms.CopyTo(ds);
-        }
-
-        public DepotManifest ConvertToSteamManifest(uint depotId)
-        {
-            ulong uncompressedSize = 0, compressedSize = 0;
-            var newManifest = new DepotManifest();
-            newManifest.Files = new List<DepotManifest.FileData>(Files.Count);
-
-            foreach (var file in Files)
-            {
-                var fileNameHash = SHA1.HashData(Encoding.UTF8.GetBytes(file.FileName.Replace('/', '\\').ToLowerInvariant()));
-                var newFile = new DepotManifest.FileData(file.FileName, fileNameHash, file.Flags, file.TotalSize, file.FileHash, null, false, file.Chunks.Count);
-
+            if (file.Chunks != null)
                 foreach (var chunk in file.Chunks)
                 {
-                    var newChunk = new DepotManifest.ChunkData(chunk.ChunkID, BitConverter.ToUInt32(chunk.Checksum, 0), chunk.Offset, chunk.CompressedLength, chunk.UncompressedLength);
+                    var newChunk = new DepotManifest.ChunkData(chunk.ChunkId, BitConverter.ToUInt32(chunk.Checksum, 0),
+                        chunk.Offset, chunk.CompressedLength, chunk.UncompressedLength);
                     newFile.Chunks.Add(newChunk);
 
                     uncompressedSize += chunk.UncompressedLength;
                     compressedSize += chunk.CompressedLength;
                 }
 
-                newManifest.Files.Add(newFile);
-            }
-
-            newManifest.FilenamesEncrypted = false;
-            newManifest.DepotID = depotId;
-            newManifest.ManifestGID = ID;
-            newManifest.CreationTime = CreationTime;
-            newManifest.TotalUncompressedSize = uncompressedSize;
-            newManifest.TotalCompressedSize = compressedSize;
-            newManifest.EncryptedCRC = 0;
-
-            return newManifest;
+            newManifest.Files.Add(newFile);
         }
+
+        newManifest.FilenamesEncrypted = false;
+        newManifest.DepotID = depotId;
+        newManifest.ManifestGID = Id;
+        newManifest.CreationTime = CreationTime;
+        newManifest.TotalUncompressedSize = uncompressedSize;
+        newManifest.TotalCompressedSize = compressedSize;
+        newManifest.EncryptedCRC = 0;
+
+        return newManifest;
+    }
+
+    [ProtoContract]
+    public class FileData
+    {
+        // Proto ctor
+        private FileData()
+        {
+            Chunks = [];
+        }
+
+        public FileData(DepotManifest.FileData sourceData) : this()
+        {
+            FileName = sourceData.FileName;
+            sourceData.Chunks.ForEach(c => Chunks.Add(new ChunkData(c)));
+            Flags = sourceData.Flags;
+            TotalSize = sourceData.TotalSize;
+            FileHash = sourceData.FileHash;
+        }
+
+        [ProtoMember(1)] public string FileName { get; internal set; }
+
+        /// <summary>
+        ///     Gets the chunks that this file is composed of.
+        /// </summary>
+        [ProtoMember(2)]
+        public List<ChunkData> Chunks { get; private set; }
+
+        /// <summary>
+        ///     Gets the file flags
+        /// </summary>
+        [ProtoMember(3)]
+        public EDepotFileFlag Flags { get; private set; }
+
+        /// <summary>
+        ///     Gets the total size of this file.
+        /// </summary>
+        [ProtoMember(4)]
+        public ulong TotalSize { get; private set; }
+
+        /// <summary>
+        ///     Gets the hash of this file.
+        /// </summary>
+        [ProtoMember(5)]
+        public byte[] FileHash { get; private set; }
+    }
+
+    [ProtoContract(SkipConstructor = true)]
+    public class ChunkData(DepotManifest.ChunkData sourceChunk)
+    {
+        /// <summary>
+        ///     Gets the SHA-1 hash chunk id.
+        /// </summary>
+        [ProtoMember(1)]
+        public byte[] ChunkId { get; private set; } = sourceChunk.ChunkID;
+
+        /// <summary>
+        ///     Gets the expected Adler32 checksum of this chunk.
+        /// </summary>
+        [ProtoMember(2)]
+        public byte[] Checksum { get; private set; } = BitConverter.GetBytes(sourceChunk.Checksum);
+
+        /// <summary>
+        ///     Gets the chunk offset.
+        /// </summary>
+        [ProtoMember(3)]
+        public ulong Offset { get; private set; } = sourceChunk.Offset;
+
+        /// <summary>
+        ///     Gets the compressed length of this chunk.
+        /// </summary>
+        [ProtoMember(4)]
+        public uint CompressedLength { get; private set; } = sourceChunk.CompressedLength;
+
+        /// <summary>
+        ///     Gets the decompressed length of this chunk.
+        /// </summary>
+        [ProtoMember(5)]
+        public uint UncompressedLength { get; private set; } = sourceChunk.UncompressedLength;
     }
 }
